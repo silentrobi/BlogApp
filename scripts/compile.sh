c6fdc6f12d4b78ed6958be9edda63401a917f7ff
Mohammad  Abu Musa Rabiul <mohammad.rabiul@bordatech.com>
2021-02-03 18:14:42 +0300

    change email server credentials as env

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,6 +7,8 @@ cd $pwd
 echo $pwd
 
 EMAIL_PASSWORD=`printenv EMAIL_PASSWORD`
+EMAIL_SERVER=`printenv EMAIL_SERVER`
+EMAIL_NAME=`printenv EMAIL_NAME`
 
 # run infrastructure.yml file
 docker-compose -f ../composes/infrastructure.yml up -d
